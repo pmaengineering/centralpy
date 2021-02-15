a347c68d1553f0726dab2c36fa911e6f64d42e52
James K. Pringle <jameskpringle@gmail.com>
2021-02-15 12:30:22 -0600

    Refine command line options for centralpy command.

--- centralpy/cli.py
+++ centralpy/cli.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 import click
 
@@ -6,38 +7,49 @@ from centralpy import CentralClient
 from centralpy.__version__ import __version__
 
 
+logger = logging.getLogger(__name__)
+
+
 @click.group()
-@click.option('--server-url', type=str, help="The URL for the ODK Central server")
+@click.option('--url', type=str, help="The URL for the ODK Central server")
 @click.option('--email', type=str, help="An ODK Central user email")
 @click.option('--password', type=str, help="The password for the account")
 @click.option('--log-file', type=click.Path(dir_okay=False), help="Where to save logs. If not provided, then logs are not saved.")
-@click.option('--verbose', is_flag=True, help="Display logging messages to console")
-@click.option('--config', type=click.File(), help="A configuration file with KEY=VALUE defined. Keys should be formatted as CENTRAL_***")
+@click.option('--verbose', is_flag=True, help="Display logging messages to console. This cannot be enabled from a config file.")
+@click.option('--config-file', type=click.File(), help="A configuration file with KEY=VALUE defined (one per line). Keys should be formatted as CENTRAL_***.")
 @click.pass_context
-def main(ctx, server_url, email, password, log_file, config, verbose):
-    """Configure the Central command-line tool with server URL and credentials.
-    
-    Values passed as parameters take precedence over values in a config file.
+def main(ctx, url, email, password, log_file, verbose, config_file):
+    """This is centralpy, an ODK Central command-line tool.
+
+    Configure centralpy with a URL and credentials using command-line
+    parameters or from a config file. Values passed as parameters on the
+    command line take precedence over values in a config file.
     """
-    config_dict = {}
-    if config:
-        config_dict = parse_config(config)
-    if server_url:
-        config_dict['CENTRAL_URL'] = server_url
-    if email:
-        config_dict['CENTRAL_EMAIL'] = email
-    if password:
-        config_dict['CENTRAL_PASSWORD'] = password
-    if log_file:
-        config_dict['CENTRAL_LOG_FILE'] = log_file
-    ctx.ensure_object(dict)
-    client = CentralClient(
-        config_dict.get('CENTRAL_URL'),
-        config_dict.get('CENTRAL_EMAIL'),
-        config_dict.get('CENTRAL_PASSWORD'),
+    if ctx.invoked_subcommand == 'version':
+        return
+    config = get_centralpy_config(
+        config_file,
+        CENTRAL_URL=url,
+        CENTRAL_EMAIL=email,
+        CENTRAL_PASSWORD=password,
+        CENTRAL_LOG_FILE=log_file,
+        CENTRAL_VERBOSE=verbose,
     )
-    ctx.obj['client'] = client
-    setup_logging(config_dict.get("CENTRAL_LOG_FILE"))
+    setup_logging(config.get("CENTRAL_LOG_FILE"), config.get("CENTRAL_VERBOSE"))
+    try:
+        client = CentralClient(
+            config['CENTRAL_URL'],
+            config['CENTRAL_EMAIL'],
+            config['CENTRAL_PASSWORD'],
+        )
+        ctx.ensure_object(dict)
+        ctx.obj['client'] = client
+    except KeyError as err:
+        print(f'Sorry, unable to create an ODK Central client because of missing information: {err!s}.')
+        print('Try adding this information to a config file or pass it as a command-line option.')
+        print('Type "centralpy --help" for more.')
+        sys.exit(1)
+
 
 
 @main.command()
@@ -65,10 +77,32 @@ def version():
     print(f"centralpy v{__version__}")
 
 
-def parse_config(config_file):
-    return {
-        k.strip(): v.strip() for k, v in (line.split("=", 1) for line in config_file if "=" in line)
-    }
+def get_centralpy_config(config_file, **kwargs):
+    config = {}
+    if config_file:
+        for line in config_file:
+            if "=" in line:
+                key, value = line.split("=", 1)
+                config[key.strip()] = value.strip()
+    for key, value in kwargs.items():
+        if key.startswith("CENTRAL_") and value is not None:
+            config[key] = value
+    return config
 
-def setup_logging(log_file):
-    logging.basicConfig(filename=log_file, level=logging.INFO)
+
+def setup_logging(log_file, verbose):
+    centralpy_logger = logging.getLogger('centralpy')
+    centralpy_logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    if verbose:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.NOTSET)
+        stream_handler.setFormatter(formatter)
+        centralpy_logger.addHandler(stream_handler)
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.NOTSET)
+        file_handler.setFormatter(formatter)
+        centralpy_logger.addHandler(file_handler)
+    if verbose or log_file:
+        logger.info("Logging is enabled!")
